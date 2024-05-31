49422900bc1c012f4e1c77eecac1d9e188cfee7b
lisatwyw <38703113+lisatwyw@users.noreply.github.com>
2024-05-31 14:48:43 -0700

    Update main.py

--- app/main.py
+++ app/main.py
@@ -2,6 +2,12 @@
 import streamlit as st
 import pandas as pd
 
+import sys, os
+
+parent_dir = os.path.dirname( os.path.realpath( __file__))
+gparent_dir = os.path.dirname( parent_dir )
+
+
 dat = pd.read_csv( '2023_NB_Canada.csv' )
 
 mkd = '''
