71ce7be26d3e09b2a76e214fe71be58466276f9c
lisatwyw <38703113+lisatwyw@users.noreply.github.com>
2024-05-31 14:53:47 -0700

    Update main.py

--- app/main.py
+++ app/main.py
@@ -4,19 +4,25 @@ import pandas as pd
 from pathlib import Path
 import sys, os
 
+
+# ================================ config ================================ 
+st.set_page_config(layout="wide") 
+
+# ================================ admin ================================ 
 parent_dir = os.path.dirname( os.path.realpath( __file__))  # /mount/src/postpostdoc/app/main.py 
 gparent_dir = os.path.dirname( parent_dir )
 
-st.write( gparent_dir )
-st.write( parent_dir)
-
+# ================================ data ================================ 
 f =  Path( parent_dir, '2023_NB_Canada.csv.csv' )
-st.write( f )
-
 dat = pd.read_csv(f )
 
-mkd = '''
+st.write( gparent_dir )
+st.write( parent_dir)
+st.write( f )
 
+# ================================ app body ================================ 
+
+mkd = '''
 # Happily after PhD
 
 ## About
